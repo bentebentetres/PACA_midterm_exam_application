a5c8b5eebbf6250b411d7913c9aaf3526b6ba05e
Mero <bentebentetres>
2025-02-20 20:53:03 +0800

    Controller and View

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    private $movies = [
+        'Inception', 'The Matrix', 'Interstellar', 'The Dark Knight', 'Avengers: Endgame'
+    ];
+
+    public function showMovies()
+    {
+        return view('products', ['movies' => $this->movies]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('products');
 });
+
+use App\Http\Controllers\ProductController;
+
+Route::get('/products/{theme}', [ProductController::class, 'showProducts']);
